cdf0d3bdc555d3ceedda81690014184736f71aa6
dpsmnsk <dpsmnsk@gmail.com>
2022-02-17 22:51:13 +0500

    update

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
-import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+import "reflect-metadata";
+import express from "express";
 import microConfig from "./mikro-orm.config";
+import { __prod__ } from "./constants";
+import { MikroORM } from "@mikro-orm/core";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
+    
     const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
 
-    const post = orm.em.create(Post, { title: 'my first post' });
-    await orm.em.persistAndFlush(post);
+    // const post = orm.em.create(Post, { title: 'my first post' });
+    // await orm.em.persistAndFlush(post);
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [ HelloResolver, PostResolver ],
+            validate: false
+        }),
+        context: () => ({ em: orm.em })
+    })
+
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => { `server started on localhost: 4000` })
 };
 
 main().catch(err => { console.log('err === ', err) });
