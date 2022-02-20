e36fdcd45f1e7a6d209cd6cd49460c7ca1ce252c
dpsmnsk <dpsmnsk@gmail.com>
2022-02-20 15:29:16 +0500

    register resolver

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,4 +1,5 @@
 import { Post } from "./entities/Post";
+import { User } from "./entities/User";
 import { __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
@@ -8,7 +9,7 @@ export default {
         path: path.join(__dirname, './migrations'),
         pattern: /^/,
     },
-    entities: [Post],
+    entities: [Post, User],
     dbName: 'lireddit',
     password: '',
     debug: !__prod__,
