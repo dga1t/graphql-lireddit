b34ff5691eb1346e36d6c1b76dc0a7155b103aee
dpsmnsk <dpsmnsk@gmail.com>
2022-02-23 19:51:58 +0500

    login resolver ready

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -29,18 +29,51 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
-    @Mutation(() => User)
+    @Mutation(() => UserResponse)
     async register(
         @Arg("options", () => UsernamePasswordInput) options: UsernamePasswordInput,
         @Ctx() { em }: MyContext
-    ) {
+    ): Promise<UserResponse> {
+        if (options.username.length < 2) {
+            return {
+                errors: [{
+                    field: 'username',
+                    message: 'length must be greater than 2'
+                }]
+            }
+        }
+
+        if (options.password.length <= 2) {
+            return {
+                errors: [{
+                    field: 'password',
+                    message: 'length must be greater than 2'
+                }]
+            }
+        }
+
         const hashedPassword = await argon2.hash(options.password);
         const user = em.create(User, {
             username: options.username,
             password: hashedPassword,
         });
-        await em.persistAndFlush(user);
-        return user;
+
+        try {
+            await em.persistAndFlush(user);
+        } catch(error) {
+            // || error.detail.includes('already exists')
+            // duplicate username error
+            if (error.code === '23505') {
+                return {
+                    errors: [{
+                        field: 'username',
+                        message: 'username already been taken'
+                    }]
+                }
+            }
+            console.log('message: ', error.message);
+        }
+        return { user };
     }
 
     @Mutation(() => UserResponse)
